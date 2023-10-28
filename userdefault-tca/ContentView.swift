1001234bb63bdfbec7fe20e0dd146f8bc3b3d68f
Yifan Jin <yijin@expedia.com>
2023-10-28 16:04:35 +1000

    Initial Commit

---
+++ userdefault-tca/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  userdefault-tca
+//
+//  Created by Yifan Jin on 28/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ userdefault-tca/userdefault_tcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  userdefault_tcaApp.swift
+//  userdefault-tca
+//
+//  Created by Yifan Jin on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct userdefault_tcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
